b54a729f9e3d3b01bf57fb30137ccec63474a768
Stian Jørgensrud <stianahj@gmail.com>
2020-12-27 17:54:53 +0100

    move toolbox

--- src/components/SecretCard.js
+++ src/components/SecretCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
-import { getFileNameFromPath } from "../toolbox";
+import { getFileNameFromPath } from "../util/toolbox";
 
 function SecretCard(props) {
   return (
